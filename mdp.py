0b1c2de0fe7cc5571ae73936e8b1c63565ebbf8b
EliEli000 <elieslaurent16@gmail.com>
2024-03-11 00:26:26 +0100

    Optimisation du code, ajout d'un salt à la fonction de hachage

--- mdp.py
+++ mdp.py
@@ -2,6 +2,7 @@ import random
 import string
 import pyfiglet
 import hashlib
+from hashlib import pbkdf2_hmac
 
 
 bvn = pyfiglet.figlet_format("Bienvenue")
@@ -39,11 +40,7 @@ def linux():
 			print("Merci d'entrer une valeur numérique.")
 		else:
 			if 10 <= choix <=  100 :
-				taille = choix
-				caracteres = "~,@,_,/,+,:,;,=,+,.,%,*,^,#"
-				mdp = string.ascii_letters + string.digits + caracteres
-				mdp_genere = ''.join(random.choice(mdp) for i in range(choix))
-				print("Voici votre nouveau mot de passe :" ,mdp_genere)
+				taille = choix 
 
 
 
@@ -53,6 +50,18 @@ def linux():
 					print("VOUS DEVEZ CHOISIR UN NOMBRE DE CARACTÈRE SUPÉRIEUR À 10.")
 				elif choix > 100 :
 					print("VOUS DEVEZ CHOISIR UN NOMBRE DE CARACTÈRE INFÉRIEUR À 100")
+def hashage():
+	choix = input("Merci d'entrer le mot de passe à hacher  : \n \n ")
+	choix_encodage = choix.encode('utf-8')
+	salt = 'pd'.encode()
+	our_app_iters = 500_000
+	mdp = hashlib.pbkdf2_hmac('sha256',choix_encodage,salt,our_app_iters)
+	hachage = mdp.hex()
+
+
+
+	print("Voici votre hashage :",hachage)
+
 
 
 
@@ -60,21 +69,40 @@ def linux():
 
 def debut():
 	try:
-		choix = int(input("Merci de préciser si vous voulez générer un mot de passe pour linux , ou un mot de passe pour windows et sites webs  \n \n TAPEZ 1 POUR UN MOT DE PASSE WINDOWS ET SITES WEBS, TAPEZ 2 POUR MOT DE PASSE LINUX: \n \n"))
+		choix = int(input("Merci de préciser si vous voulez générer un mot de passe pour linux , ou un mot de passe pour windows et sites webs, ou activer le mode hashe  \n \n TAPEZ 1 POUR UN MOT DE PASSE WINDOWS ET SITES WEBS, TAPEZ 2 POUR MOT DE PASSE LINUX, TAPEZ 3 POUR LE MODE HASHE : \n \n"))
 	except ValueError as e :
 		print("Erreur")
 	else:
-		if choix > 2 :
-			print("Vous devez choisir une valeur entre 1 et 2")
-		elif choix < 1 :
-			print("Vous devez choisir une valeur entre 1 et 2")
+		if choix > 3 or choix < 1 :
+			print("Vous devez choisir une valeur entre 1 et 3")
 		elif choix == 1 :
 			windows_site()
 		elif choix == 2 :
 			linux()
+		else :
+			hashage()
 
 
 debut()
 
 
-d
+
+
+#rajouter moyen de choisir entre mot de passe pour linux (plus de caractères disponibles) et mot de passe pur windows (moins de disponibles) > FAIT
+#Rajouter la fonction qui genere aleatoirement mdp pour linux (rajouter donc les caracteres spéciaux) > FAIT
+
+#nouvelles idées ?
+#peut être essayer de rajouter un cracker de mdp, ou le moyen de transformer un mot de passe en hashe
+#https://docs.python.org/3/library/hashlib.html (lis ça en vrai )
+#https://thepythoncode.com/article/crack-hashes-in-python
+
+
+#utiliser != pour simplifier le programme ainsi que liste
+
+
+
+
+
+
+
+
